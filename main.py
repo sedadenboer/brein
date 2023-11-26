9b0341b251be8d761af108f1b045c0de4e7c1793
Seda <52498111+sedadenboer@users.noreply.github.com>
2023-11-26 17:12:12 +0100

    improved baseline code

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 import vtk
-from vtkmodules.vtkFiltersSources import vtkSphereSource  
 
 
-SIMULATION = ['no-network', 'disable', 'stimulus', 'calcium']
-
-
-def read_positions(sim_type: str) -> vtk.vtkPoints:
+def get_positions(sim_type: str) -> vtk.vtkPoints:
     """Read the position file.
 
     Args:
@@ -17,13 +13,13 @@ def read_positions(sim_type: str) -> vtk.vtkPoints:
     # Get the position file location
     position_file_loc = f'data/viz-{sim_type}/positions/rank_0_positions.txt'
 
+    # Create an empty vtkPoints object
+    points = vtk.vtkPoints()
+
     # Open the position file
     with open(position_file_loc, 'r') as position_file:
         # Read the lines of the file
         lines = position_file.readlines()
-        
-        # Create an empty vtkPoints object
-        points = vtk.vtkPoints()
         
         # Iterate over the lines
         for line in lines:
@@ -41,10 +37,9 @@ def read_positions(sim_type: str) -> vtk.vtkPoints:
             points.InsertNextPoint(x, y, z)
         
         # Print the vtkPoints object
-        return points
+    return points
 
-
-def connection_reader(sim_type: str, step: int) -> dict:
+def get_connections_dict(sim_type: str, step: int) -> dict:
     """Read the connection file.
 
     Args:
@@ -57,13 +52,13 @@ def connection_reader(sim_type: str, step: int) -> dict:
     # Get the connection file location
     connection_infile_loc = f'data/viz-{sim_type}/network/rank_0_step_{step}_in_network.txt'
 
+    # Create an empty dictionary
+    connections_dict = {}
+
     # Open the connection file
     with open(connection_infile_loc, 'r') as connection_infile:
         # Read the lines of the file
         lines = connection_infile.readlines()
-        
-        # Create an empty dictionary
-        connection_dict = {}
         
         # Iterate over the lines
         for line in lines:
@@ -79,137 +74,178 @@ def connection_reader(sim_type: str, step: int) -> dict:
             fields = line.strip().split()
             
             # Extract the values
-            value = int(fields[1]) - 1
-            key = int(fields[3]) - 1
+            id1 = int(fields[1]) - 1
+            id2 = int(fields[3]) - 1
             
-            # Add the value to the list in the dictionary
-            if key in connection_dict:
-                connection_dict[key].append(value)
-            else:
-                connection_dict[key] = [value]
-        
-        # Return the connection dictionary
-        return connection_dict
+            # Add the connection to the dictionary
+            if id1 not in connections_dict:
+                connections_dict[id1] = []
+            connections_dict[id1].append(id2)
+    
+    return connections_dict
 
-def create_polydata(points, connections):
-    """Create a vtkPolyData object with points and connections.
+def get_connections(sim_type: str, step: int) -> vtk.vtkCellArray:
+    """Get the connections.
 
     Args:
-        points (vtk.vtkPoints): position points
-        connections (dict): connection dictionary
+        sim_type (str): simulation type
+        step (int): simulation step
 
     Returns:
-        vtk.vtkPolyData: vtkPolyData object
+        vtk.vtkCellArray: connections
     """
-    polydata = vtk.vtkPolyData()
-    polydata.SetPoints(points)
+    # Get the connections dictionary
+    connections_dict = get_connections_dict(sim_type, step)
 
-    lines = vtk.vtkCellArray()
-    for point_id, connected_points in connections.items():
-        for connected_point_id in connected_points:
-            line = vtk.vtkLine()
-            line.GetPointIds().SetId(0, point_id)
-            line.GetPointIds().SetId(1, connected_point_id)
-            lines.InsertNextCell(line)
+    # Create an empty vtkCellArray object to store the connections
+    connections = vtk.vtkCellArray()
 
-    polydata.SetLines(lines)
-    return polydata
+    # Iterate over the connections dictionary to get connected ids
+    for id1, id2s in connections_dict.items():
+        for id2 in id2s:
+            # Create a vtkPolyLine object to store the connection
+            polyline = vtk.vtkPolyLine()
+            polyline.GetPointIds().SetNumberOfIds(2)
 
+            # Add the ids to the vtkPolyLine object
+            polyline.GetPointIds().SetId(0, id1)
+            polyline.GetPointIds().SetId(1, id2)
+            
+            # Add the vtkPolyLine object to the vtkCellArray object
+            connections.InsertNextCell(polyline)
+            
+    return connections
 
-def create_glyph_filter(polydata):
-    """Create a glyph filter to show individual points.
+def create_polydata(points: vtk.vtkPoints, connections: vtk.vtkCellArray) -> (
+        vtk.vtkPolyData, vtk.vtkPolyData
+        ):
+    """Create polydata for points and connections.
 
     Args:
-        polydata (vtk.vtkPolyData): vtkPolyData object
+        points (vtk.vtkPoints): points
+        connections (vtk.vtkCellArray): connections
+    
+    Returns:
+        vtk.vtkPolyData: points polydata
+        vtk.vtkPolyData: connections polydata
+    """
+    # Create vtkPolyData objects for points and connections
+    points_polydata = vtk.vtkPolyData()
+    lines_polydata = vtk.vtkPolyData()
 
+    # Set the points and connections for both vtkPolyData objects
+    points_polydata.SetPoints(points)
+    lines_polydata.SetPoints(points)
+    
+    # Set the lines for the connections vtkPolyData object
+    lines_polydata.SetLines(connections)
+
+    return points_polydata, lines_polydata
+
+def create_glyph_filter(polydata: vtk.vtkPolyData) -> vtk.vtkGlyph3D:
+    """Create a glyph filter to display all the points.
+
+    Args:
+        polydata (vtk.vtkPolyData): polydata
+    
     Returns:
         vtk.vtkGlyph3D: glyph filter
     """
-    sphere_source = vtkSphereSource()
-
+    # Create a sphere source and a glyph filter to represent and display the points
+    sphere_source = vtk.vtkSphereSource()
     glyph_filter = vtk.vtkGlyph3D()
+
+    # Set the input data for the glyph filter
     glyph_filter.SetInputData(polydata)
     glyph_filter.SetSourceConnection(sphere_source.GetOutputPort())
-    glyph_filter.SetScaleModeToScaleByScalar()
-    glyph_filter.SetScaleFactor(3)
+    glyph_filter.SetScaleFactor(5)
 
     return glyph_filter
 
-
-def create_tube_filter(polydata):
-    """Create a tube filter to create tubes around the connections.
-
-    Args:
-        polydata (vtk.vtkPolyData): vtkPolyData object
-
-    Returns:
-        vtk.vtkTubeFilter: tube filter
-    """
-    tube_filter = vtk.vtkTubeFilter()
-    tube_filter.SetInputData(polydata)
-    tube_filter.SetRadius(1)
-
-    return tube_filter
-
-
-def create_renderer(glyph_filter, tube_filter):
-    """Create a renderer with actors for tubes and points.
+def create_point_actor(glyph_filter: vtk.vtkGlyph3D) -> vtk.vtkActor:
+    """Create an actor for the points.
 
     Args:
-        polydata (vtk.vtkPolyData): vtkPolyData object
         glyph_filter (vtk.vtkGlyph3D): glyph filter
-        tube_filter (vtk.vtkTubeFilter): tube filter
-
+    
     Returns:
-        vtk.vtkRenderer: renderer
+        vtk.vtkActor: point actor
     """
-    tube_mapper = vtk.vtkPolyDataMapper()
-    tube_mapper.SetInputConnection(tube_filter.GetOutputPort())
-
-    tube_actor = vtk.vtkActor()
-    tube_actor.SetMapper(tube_mapper)
-    tube_actor.GetProperty().SetColor(1, 1, 1)
-
+    # Create a mapper and an actor for the points
     point_mapper = vtk.vtkPolyDataMapper()
     point_mapper.SetInputConnection(glyph_filter.GetOutputPort())
-
     point_actor = vtk.vtkActor()
     point_actor.SetMapper(point_mapper)
-    point_actor.GetProperty().SetColor(0, 0, 1)
 
-    renderer = vtk.vtkRenderer()
-    renderer.AddActor(tube_actor)
-    renderer.AddActor(point_actor)
+    # Appearance settings for the points
+    point_actor.GetProperty().SetColor(0.8, 0.2, 0.2)
+    return point_actor
 
-    return renderer
-
-
-def plot_3d(points, connections):
-    """Plot the points and connections in 3D.
+def create_connection_actor(polydata: vtk.vtkPolyData) -> vtk.vtkActor:
+    """Create an actor for the connections.
 
     Args:
-        points (vtk.vtkPoints): position points
-        connections (dict): connection dictionary
+        polydata (vtk.vtkPolyData): polydata
+    
+    Returns:
+        vtk.vtkActor: connections actor
     """
-    polydata = create_polydata(points, connections)
-    glyph_filter = create_glyph_filter(polydata)
-    tube_filter = create_tube_filter(polydata)
-    renderer = create_renderer(glyph_filter, tube_filter)
+    # Create a mapper and an actor for the connections
+    connection_mapper = vtk.vtkPolyDataMapper()
+    connection_mapper.SetInputData(polydata)
+    connection_actor = vtk.vtkActor()
+    connection_actor.SetMapper(connection_mapper)
 
+    # Appearance settings for the connections
+    connection_actor.GetProperty().SetLineWidth(2)
+    connection_actor.GetProperty().SetRenderLinesAsTubes(1)
+    connection_actor.GetProperty().SetOpacity(0.8)
+
+    return connection_actor
+
+def plot_basic(positions: vtk.vtkPoints, connections: vtk.vtkCellArray) -> None:
+    """Plot the points and connections.
+
+    Args:
+        positions (vtk.vtkPoints): position points
+        connections (vtk.vtkCellArray): connections
+
+    Returns:
+        None
+    """
+    # Create polydata for points and connections
+    points, lines = create_polydata(positions, connections)
+    # Create a glyph filter to display all the points
+    glyph_filter = create_glyph_filter(points)
+
+    # Create actors for points and connections
+    point_actor = create_point_actor(glyph_filter)
+    connection_actor = create_connection_actor(lines)
+
+    # Create a renderer and add the actors
+    renderer = vtk.vtkRenderer()
+    renderer.AddActor(point_actor)
+    renderer.AddActor(connection_actor)
+    renderer.ResetCamera()
+
+    # Create a render window and add the renderer
     render_window = vtk.vtkRenderWindow()
     render_window.AddRenderer(renderer)
 
+    # Create an interactor
     render_window_interactor = vtk.vtkRenderWindowInteractor()
     render_window_interactor.SetRenderWindow(render_window)
 
+    # Render the scene and start the interactor
     render_window.Render()
     render_window_interactor.Start()
 
-
 def main():
-    positions = read_positions(SIMULATION[2])
-    connections = connection_reader(SIMULATION[2], 0)
-    plot_3d(positions, connections)
+    # Choose the simulation type
+    SIMULATION = ['no-network', 'disable', 'stimulus', 'calcium']
+    positions = get_positions(SIMULATION[1])
+    connections = get_connections(SIMULATION[1], 0)
+    plot_basic(positions, connections)
 
 
 if __name__ == '__main__':
